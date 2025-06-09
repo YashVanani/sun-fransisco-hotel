6a0ce5f9a070178f3ba29adbdf838bce6902b360
yashvanani <vananiyash9828@gmail.com>
2025-06-09 23:46:59 +0530

    applied changes for room details and etc

--- app/account/history/page.js
+++ app/account/history/page.js
@@ -1,22 +1,54 @@
-import styles from "./style.module.css";
+import { useAuth } from "@/app/_context/AuthContext";
+import { db } from "@/app/_lib/firebase/firebase";
 import Heading from "@/app/_ui/Heading";
-import ReservationCard from "./_components/ReservationCard";
-import { getGuestReservations } from "@/app/_lib/supabase/reservations";
-import { auth } from "@/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import Link from "next/link";
+import ReservationCard from "./_components/ReservationCard";
+import styles from "./style.module.css";
 
 export const metadata = {
   title: "Booking History",
   description: "Reservations history at the Hotel Booking App ",
 };
 
+async function fetchUserReservations(userId) {
+  console.log("userId ::::::", userId);
+  try {
+    const bookingsRef = collection(db, "room_bookings");
+    const q = query(bookingsRef, where("user_id", "==", userId));
+    const snapshot = await getDocs(q);
+    console.log("snapshot:::::", snapshot);
+
+    const reservations = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log("reservations :::::::", reservations);
+    return reservations;
+  } catch (error) {
+    console.log("Error fetching reservations:", error);
+    return [];
+  }
+}
+
 async function History() {
-  let session = {};
   let reservations = [];
   try {
-    session = await auth();
-    console.log({ HISTORY_SESSION: session });
-    reservations = (await getGuestReservations(session.user?.id)) ?? [];
+    const { user } = useAuth();
+
+    if (!user) {
+      console.log("No user is logged in.");
+      return (
+        <>
+          <Heading textClassName={styles.heading}>Your History</Heading>
+          <p>Please sign in to view your booking history.</p>
+        </>
+      );
+    }
+
+    const userId = user.uid;
+    reservations = await fetchUserReservations(userId);
+    console.log("reservations :::::::::", reservations);
   } catch (err) {
     console.log(err);
   }
@@ -26,7 +58,9 @@ async function History() {
       <Heading textClassName={styles.heading}>Your History</Heading>
       <div>
         {reservations.length ? (
-          reservations.reverse().map((item) => <ReservationCard key={item.id} reservation={item} />)
+          reservations
+            .reverse()
+            .map((item) => <ReservationCard key={item.id} reservation={item} />)
         ) : (
           <div>
             <p>You have no booked room.</p>
